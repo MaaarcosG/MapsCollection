b2a568e8bc344cae5fca098b173186c89d807d96
Alejandro-PC\Alejandro <mon17014@uvg.edu.gt>
2018-03-08 19:47:57 -0600

    Mejoramiento del menu del main

--- src/Main.java
+++ src/Main.java
@@ -13,6 +13,7 @@ public class Main {
 	public static void main(String[] args) {
 		String datos = "";
 		MapFactory maps = new MapFactory();
+		
 		/*Abrimos el Archivo*/
 		try {
 			/*Abrimos el archivo de texto*/
@@ -45,14 +46,51 @@ public class Main {
 		System.out.println("Escriba el tipo de Map que desea utilizar: ");
 		String Cartatipo = teclado.nextLine();
 		
+		boolean correcto =false;
 		/*Condicion para cada uno de los metodos*/
 		if(Cartatipo.equals("HashMap")) {
 			System.out.println("-----");
 			maps.Escoger(Cartatipo);
-			System.out.println("Ingrese el nombre de la carta que desea agregar: ");
-			String nombre = teclado.nextLine();
-			maps.cartasEnArray();
+			correcto = true;
 			
 		}
+		else {
+			if(Cartatipo.equals("TreeMap")) {
+				System.out.println("-----");
+				maps.Escoger(Cartatipo);
+				correcto = true;
+				
+			}
+			else {
+				if(Cartatipo.equals("LinkedMap")) {
+					System.out.println("-----");
+					maps.Escoger(Cartatipo);
+					correcto = true;
+				}
+				else {
+					System.out.println("Debe escribir el nombre correctamente, gracias");
+				}
+			}
+		}
+		while (correcto){
+			System.out.println("¿Qué operación desea realizar? "
+					+ "\n 1. Agregar carta \n 2. Indicar tipo \n 3. Mostrar el nombre, tipo y cantidad"
+					+ "\n 4. Mostrar el nombre, tipo y cantidad ordenadas por tipo\n"
+					+ " 5. Mostrar el nombre y tipo de cartas existentes "
+					+ "\n 6. Mostrar el nombre y tipo de cartas existentes, ordenadas por tipo"
+					+ "\nIngrese el número de la operación deseada");
+			String operacion = teclado.nextLine();
+			if (operacion.equals("1")) {
+				//System.out.println("Ingrese el nombre de la carta que desea agregar: ");
+				//String nombre = teclado.nextLine();
+				//maps.cartasEnArray();
+			}
+			System.out.println("¿Desea continuar? escriba si o no.");
+			String continuar = teclado.nextLine();
+			if (continuar.equals("no")) {
+				correcto=false;
+			}
+		}
+		System.out.println("Feliz Día");
 	}
 }
